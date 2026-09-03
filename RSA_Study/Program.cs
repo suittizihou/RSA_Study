c407b8338372b6c587897e3ee14e806600322212
suittizihou <suittizihou@gmail.com>
2026-09-04 00:54:20 +0900

    namespaceを先頭に記述

--- RSA_Study/Program.cs
+++ RSA_Study/Program.cs
@@ -1,4 +1,6 @@
-﻿public static class MyMath
+﻿namespace RSA_Study;
+
+public static class MyMath
 {
     /// <summary>
     /// 暗号・復号用の累乗計算
@@ -51,81 +53,78 @@ public class PublicKey(int n, int e)
     }
 }
 
-namespace RSA_Study
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int p = 11;
+        int q = 5;
+        
+        int n = p * q;
+        int nPhi = (p - 1) * (q - 1);
+        int e = CalcE(nPhi);
+        int d = CalcD(e, nPhi);
+
+        var pubKey = new PublicKey(n, e);
+        var priKey = new PrivateKey(n, d);
+
+        int m = CreateMessage(n);
+        System.Console.WriteLine($"m = {m}");
+        int c = pubKey.Encryption(m);
+        System.Console.WriteLine($"c = {c}");
+        int Ans = priKey.Decrypt(c);
+        System.Console.WriteLine($"Ans = {Ans}");
+    }
+
+    /// <summary>
+    /// 暗号化したいメッセージを作成する
+    /// メッセージは 0 <= m < n でなければならない
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    private static int CreateMessage(int n)
+    {
+        return Random.Shared.Next(0, n);
+    }
+
+    /// <summary>
+    /// e * d を Φ(n) で割った余りが 1 になる d の値を求める
+    /// </summary>
+    /// <param name="e"></param>
+    /// <param name="nPhi">Φ(n)</param>
+    /// <returns></returns>
+    private static int CalcD(int e, int nPhi)
+    {
+        int d = 1;
+        for (; e * d % nPhi != 1; d++);
+        return d;
+    }
+
+    /// <summary>
+    /// Φ(n)と互いに素である数を最小値から順に求めていく
+    /// 最初に見つかった値を e として採用する
+    /// </summary>
+    /// <param name="nPhi"></param>
+    /// <returns></returns>
+    private static int CalcE(int nPhi)
+    {
+        for(int i = 2; i < nPhi; i++)
         {
-            int p = 11;
-            int q = 5;
-            
-            int n = p * q;
-            int nPhi = (p - 1) * (q - 1);
-            int e = CalcE(nPhi);
-            int d = CalcD(e, nPhi);
-
-            var pubKey = new PublicKey(n, e);
-            var priKey = new PrivateKey(n, d);
-
-            int m = CreateMessage(n);
-            System.Console.WriteLine($"m = {m}");
-            int c = pubKey.Encryption(m);
-            System.Console.WriteLine($"c = {c}");
-            int Ans = priKey.Decrypt(c);
-            System.Console.WriteLine($"Ans = {Ans}");
+            if (Gcd(i, nPhi) == 1) return i;
         }
+        return -1;
+    }
 
-        /// <summary>
-        /// 暗号化したいメッセージを作成する
-        /// メッセージは 0 <= m < n でなければならない
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        private static int CreateMessage(int n)
-        {
-            return Random.Shared.Next(0, n);
-        }
-
-        /// <summary>
-        /// e * d を Φ(n) で割った余りが 1 になる d の値を求める
-        /// </summary>
-        /// <param name="e"></param>
-        /// <param name="nPhi">Φ(n)</param>
-        /// <returns></returns>
-        private static int CalcD(int e, int nPhi)
-        {
-            int d = 1;
-            for (; e * d % nPhi != 1; d++);
-            return d;
-        }
-
-        /// <summary>
-        /// Φ(n)と互いに素である数を最小値から順に求めていく
-        /// 最初に見つかった値を e として採用する
-        /// </summary>
-        /// <param name="nPhi"></param>
-        /// <returns></returns>
-        private static int CalcE(int nPhi)
-        {
-            for(int i = 2; i < nPhi; i++)
-            {
-                if (Gcd(i, nPhi) == 1) return i;
-            }
-            return -1;
-        }
-
-        /// <summary>
-        /// 最大公約数を求める
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <returns></returns>
-        private static int Gcd(int a, int b)
-        {
-            int mod = a % b;
-            if (mod == 0) return b;
-            return Gcd(b, mod);
-        }
+    /// <summary>
+    /// 最大公約数を求める
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private static int Gcd(int a, int b)
+    {
+        int mod = a % b;
+        if (mod == 0) return b;
+        return Gcd(b, mod);
     }
 }
